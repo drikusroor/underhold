741d542d3a161c292a7f27270a3349bb64a2c9d0
Drikus Roor <drikusroor@gmail.com>
2021-04-16 13:42:57 +0200

    Add mvp homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,35 @@ module.exports = {
     ],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        colors: {
-            underhold: {
-                DEFAULT: "08C447",
+        extend: {
+            colors: {
+                transparent: "transparent",
+                current: "currentColor",
+                blue: {
+                    light: "#85d7ff",
+                    DEFAULT: "#1fb6ff",
+                    dark: "#009eeb",
+                },
+                pink: {
+                    light: "#ff7ce5",
+                    DEFAULT: "#ff49db",
+                    dark: "#ff16d1",
+                },
+                gray: {
+                    darkest: "#1f2d3d",
+                    dark: "#3c4858",
+                    DEFAULT: "#c0ccda",
+                    light: "#e0e6ed",
+                    lightest: "#f9fafc",
+                },
+                green: {
+                    DEFAULT: "#08c447",
+                },
+                underhold: {
+                    DEFAULT: "08C447",
+                },
             },
         },
-        extend: {},
     },
     variants: {
         extend: {},
